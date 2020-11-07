c96cc6ccb16df9faeb093fe71b3f9c3fc11ae219
Vasilios Magriplis <etameson@gmail.com>
2020-11-07 12:12:35 +1100

    Bugfixes

--- src/cpp-utils/include/Vector.hpp
+++ src/cpp-utils/include/Vector.hpp
@@ -2,18 +2,19 @@
 #include <vector>
 #include <functional>
 #include <algorithm>
+#include <optional>
 
 namespace CppUtils::Vector
 {
 	//https://thispointer.com/c-how-to-find-an-element-in-vector-and-get-its-index/
 
 	template<typename T>
-	uint64_t IndexOf(std::vector<T>& v, const std::function<bool(const T&)> predicate)
+	std::optional<size_t> IndexOf(std::vector<T>& v, const std::function<bool(const T&)> predicate)
 	{
-		auto iter = std::find_if(v.begin, v.end(), elem, predicate);
+		auto iter = std::find_if(v.begin, v.end(), predicate);
 		if (iter == v.end())
-			return -1;
-		return std::distance(v.begin(), v.end());
+			return std::nullopt_t;
+		return std::distance(v.begin(), iter);
 	}
 
 	template<typename T>
